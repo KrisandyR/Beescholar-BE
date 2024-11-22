d7e25237adffe343e928354543cc0fcd62a3b052
Krisandy Rivera <krisandy.rivera@binus.edu>
2024-11-22 11:50:00 +0700

    chore : extend classes

--- app/Models/Dialogue.php
+++ app/Models/Dialogue.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Dialogue extends Model
+class Dialogue extends Scene
 {
     use HasFactory;
 }

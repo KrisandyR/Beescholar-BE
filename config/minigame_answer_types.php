d18a6a0806ff95ce525ba86c1f6ac910c4864d4f
Krisandy Rivera <krisandy.rivera@binus.edu>
2025-01-12 09:10:26 +0700

    chore : Minigame answer type config

---
+++ config/minigame_answer_types.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Quiz Multiple Choice Answer' =>  'App\Models\QuizMultipleChoiceAnswer' ,
+    'Quiz Order Steps Answer' => 'App\Models\QuizOrderStepsAnswer',
+    'Quiz Order Steps Answer Detail' => 'App\Models\QuizOrderStepsAnswerDetail',
+    'Crossword Answer' => 'App\Models\CrosswordAnswer',
+    'Drum Puzzle Answer' => 'App\Models\DrumPuzzleAnswer',
+];
